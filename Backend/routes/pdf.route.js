4b828daa685660384852246a6b24f22c8d5f9988
HamirMandha45 <hamirmandha4577@gmail.com>
2024-12-04 11:11:25 +0530

    pdf delete functionality complete

--- Backend/routes/pdf.route.js
+++ Backend/routes/pdf.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { authenticateToken } from '../middleware/auth.middleware.js';
-import { uploadPdf } from '../controllers/pdfupload.controller.js';
+import { deletePdf, uploadPdf } from '../controllers/pdfupload.controller.js';
 import { upload } from '../middleware/fileupload.middleware.js';
 
 const router = express.Router();
 
 router.post('/uploadPdf',upload.fields([{name:'pdf'}]),authenticateToken,uploadPdf)
+router.post('/deletePdf',authenticateToken,deletePdf)
 
 export{
     router
